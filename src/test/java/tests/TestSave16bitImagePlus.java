b85ba6e89df9be85adfa07896609a046c4af2b4f
Christian Tischer <christian.tischer@embl.de>
2019-10-16 13:20:32 +0200

    Improve tests

--- src/test/java/tests/TestSave16bitImagePlus.java
+++ src/test/java/tests/TestSave16bitImagePlus.java
@@ -1,5 +1,6 @@
 package tests;
 
+import bdv.img.imaris.Imaris;
 import bdv.spimdata.SpimDataMinimal;
 import bdv.spimdata.XmlIoSpimDataMinimal;
 import bdv.util.BdvFunctions;
@@ -9,16 +10,17 @@ import ij.ImagePlus;
 import mpicbg.spim.data.SpimDataException;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class TestSave16bitImagePlus
 {
-
 	@Test
-	public void saveAsImaris( ) throws SpimDataException
+	public void saveAsImaris( ) throws SpimDataException, IOException
 	{
 		final net.imagej.ImageJ ij = new net.imagej.ImageJ();
 		ij.ui().showUI();
 
-		ImagePlus imp = IJ.openImage( TestPathConstants.TEST_FOLDER + "test_data/mri-stack-16bit.tif" );
+		ImagePlus imp = IJ.openImage( TestPathConstants.TEST_FOLDER + "test-data/mri-stack-16bit.tif" );
 		imp.setTitle( "mri-stack-16bit" );
 
 		ImarisWriter writer = new ImarisWriter( imp, TestPathConstants.TEST_FOLDER + "test-output" );
@@ -26,13 +28,13 @@ public class TestSave16bitImagePlus
 		writer.setLogService( ij.log() );
 		writer.write();
 
-		final SpimDataMinimal spimDataMinimal = new XmlIoSpimDataMinimal().load( TestPathConstants.TEST_FOLDER + "test-output/mri-stack-16bit-header.ims" );
+		final SpimDataMinimal spimDataMinimal = Imaris.openIms( TestPathConstants.TEST_FOLDER + "test-output/mri-stack-16bit-header.ims" );
 
 		BdvFunctions.show( spimDataMinimal );
 	}
 
 
-	public static void main( String[] args ) throws SpimDataException
+	public static void main( String[] args ) throws SpimDataException, IOException
 	{
 		new TestSave16bitImagePlus().saveAsImaris();
 	}
