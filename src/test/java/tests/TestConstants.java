5fcaf6652cabc5308dd21a5f15b0d03100fc275b
Jan Eglinger <jan.eglinger@gmail.com>
2020-08-11 08:45:23 +0200

    Fix pom.xml and test resource path

--- src/test/java/tests/TestConstants.java
+++ src/test/java/tests/TestConstants.java
@@ -2,6 +2,6 @@ package tests;
 
 public class TestConstants
 {
-	public static final String TEST_FOLDER = "/Users/tischer/Documents/imaris-writer/src/test/resources/";
+	public static final String TEST_FOLDER = "src/test/resources/";
 	public static boolean interactive = false;
 }
