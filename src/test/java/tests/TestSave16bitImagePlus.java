91b043819326db08cc6c51187e1a37d7516c3d0e
Christian Tischer <christian.tischer@embl.de>
2020-11-12 20:30:31 +0100

    Bump to next development cycle
    Signed-off-by: Christian Tischer <christian.tischer@embl.de>

--- src/test/java/tests/TestSave16bitImagePlus.java
+++ src/test/java/tests/TestSave16bitImagePlus.java
@@ -1,3 +1,31 @@
+/*-
+ * #%L
+ * imaris-writer
+ * %%
+ * Copyright (C) 2018 - 2020 EMBL
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 package tests;
 
 import bdv.img.imaris.Imaris;
@@ -10,7 +38,6 @@ import ij.ImagePlus;
 import ij.process.LUT;
 import mpicbg.spim.data.SpimDataException;
 import org.junit.Test;
-import org.scijava.log.LogService;
 
 import java.awt.*;
 import java.io.File;
@@ -19,7 +46,7 @@ import java.io.IOException;
 public class TestSave16bitImagePlus
 {
 	@Test
-	public void saveAsImaris() throws SpimDataException, IOException
+	public void run() throws SpimDataException, IOException
 	{
 		final String fileName = "mri-stack-16bit";
 
@@ -40,10 +67,9 @@ public class TestSave16bitImagePlus
 			BdvFunctions.show( spimDataMinimal );
 	}
 
-
 	public static void main( String[] args ) throws SpimDataException, IOException
 	{
 		TestConstants.interactive = true;
-		new TestSave16bitImagePlus().saveAsImaris();
+		new TestSave16bitImagePlus().run();
 	}
 }
--- src/test/java/tests/TestSaveShortImagePlus.java
+++ src/test/java/tests/TestSaveShortImagePlus.java
@@ -1,3 +1,31 @@
+/*-
+ * #%L
+ * imaris-writer
+ * %%
+ * Copyright (C) 2018 - 2020 EMBL
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 package tests;
 
 import de.embl.cba.imaris.ImarisWriter;
@@ -12,10 +40,8 @@ import static tests.TestConstants.TEST_FOLDER;
 public class TestSaveShortImagePlus
 {
 	@Test
-	public void saveShortImagePlusAsImaris( )
+	public void run( )
 	{
-		final net.imagej.ImageJ ij = new net.imagej.ImageJ();
-
 		final int sizeZ = 30; // set to 600 to test java indexing issues
 		final int sizeXY = 100; // set to 2048 to test java indexing issues
 		final ImageStack imageStack = new ImageStack( sizeXY, sizeXY, sizeZ );
@@ -30,19 +56,16 @@ public class TestSaveShortImagePlus
 						ip.set( x, y, x );
 		}
 
-
 		ImagePlus imp = new ImagePlus( "image", imageStack );
 		imp.setDimensions( 1, sizeZ, 1 );
 
 		ImarisWriter writer = new ImarisWriter( imp, TEST_FOLDER + "test-data" );
-		writer.setLogService( ij.log() );
 		writer.write();
 	}
 
-
 	public static void main( String[] args )
 	{
-		new TestSaveShortImagePlus().saveShortImagePlusAsImaris();
+		new TestSaveShortImagePlus().run();
 	}
 
 }
