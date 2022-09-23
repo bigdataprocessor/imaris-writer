6b949e06fca8621273b0773f64e959a3802ee903
Christian Tischer <christian.tischer@embl.de>
2022-09-23 16:04:57 +0200

    Update licenses

--- src/main/java/de/embl/cba/imaris/ImarisUtils.java
+++ src/main/java/de/embl/cba/imaris/ImarisUtils.java
@@ -2,7 +2,7 @@
  * #%L
  * imaris-writer
  * %%
- * Copyright (C) 2018 - 2020 EMBL
+ * Copyright (C) 2018 - 2022 EMBL
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
